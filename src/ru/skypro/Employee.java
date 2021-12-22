6b80c045dc786897d7ed60b418749c4178ad70b9
SergeFox02 <Fox2188@gmail.com>
2021-12-22 15:13:58 +0300

    Второй коммит. Решил курсовую работу базовой сложности.

---
+++ src/ru/skypro/Employee.java
@@ -0,0 +1,80 @@
+package ru.skypro;
+
+import java.util.Objects;
+
+public class Employee {
+
+    private final int id;
+    private static int counter;
+    private final String middleName;
+    private final String firstName;
+    private final String lastName;
+    private int department;
+    private int salary;
+
+
+    public Employee(String middleName, String firstName, String lastName, int department, int salary) {
+        this.middleName = middleName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+        this.salary = salary;
+        this.id = counter++;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "middleName='" + middleName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && department == employee.department && salary == employee.salary && middleName.equals(employee.middleName) && firstName.equals(employee.firstName) && lastName.equals(employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, middleName, firstName, lastName, department, salary);
+    }
+}
--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,93 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Employee employee[] = new Employee[10];
+
+        employee[0] = new Employee("Ivanov", "Ivan", "Ivanovich", 1, 60_000 );
+        employee[1] = new Employee("Peytorov", "Peytor", "Petrovich", 1, 65_000);
+        employee[2] = new Employee("Alexandrov", "Alexandr", "Alexandrovich", 2, 78_000);
+        employee[3] = new Employee("Krylova", "Nadegda", "Ivanovna", 2, 67_000);
+        employee[4] = new Employee("Petrova", "Ekaterina", "Danilovna", 3, 100_000);
+        employee[5] = new Employee("Sergeev", "Sergey", "Sergeevich", 4, 105_000);
+        employee[6] = new Employee("Kononova", "Julia", "Alexandrpvna", 5, 110_000);
+        employee[7] = new Employee("Danilova", "Anna", "Anatolievna", 2, 90_000);
+
+        System.out.println(employee[0]);
+        System.out.println(employee[2].getId());
+
+        printEmployee(employee);
+        totalSalaryEmployees(employee);
+        minSalaryEmployee(employee);
+        maxSalaryEmployee(employee);
+        mediumSalaryEmployee(employee);
+        printFullNameEmployees(employee);
+
+    }
+
+    public static void printEmployee(Employee[] employee) {
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null){
+                System.out.println("employee = " + employee[i]);
+            }
+        }
+    }
+
+    public static int totalSalaryEmployees(Employee[] employee) {
+        int sum = 0;
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null){
+                sum += employee[i].getSalary();
+            }
+        }
+        System.out.println("Сумма затрат на зарплаты в месяц: " + sum + " руб.");
+        return sum;
+    }
+
+    public static int minSalaryEmployee(Employee[] employee) {
+        int minSalary = employee[0].getSalary();
+        int indexEmployee = 0;
+        for (int i = 1; i < employee.length; i++) {
+            if (employee[i] != null && employee[i].getSalary() < minSalary){
+                minSalary = employee[i].getSalary();
+                indexEmployee = i;
+            }
+        }
+        System.out.println("Работник: " + employee[indexEmployee] + " получает минимальную зарплату");
+        return minSalary;
+    }
+
+    public static int maxSalaryEmployee(Employee[] employee) {
+        int maxSalary = employee[0].getSalary();
+        int indexEmployee = 0;
+        for (int i = 1; i < employee.length; i++) {
+            if (employee[i] != null && employee[i].getSalary() > maxSalary){
+                maxSalary = employee[i].getSalary();
+                indexEmployee = i;
+            }
+        }
+        System.out.println("Работник: " + employee[indexEmployee] + " получает максимальную зарплату");
+        return maxSalary;
+    }
+
+    public static float mediumSalaryEmployee(Employee[] employee) {
+        int numberEmployee = 0;
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null){
+                numberEmployee++;
+            }
+        }
+        float mediumSalary = (float) totalSalaryEmployees(employee) / numberEmployee;
+        System.out.println("Средняя зарпалата в нашей организации: " + mediumSalary + " руб.");
+        return mediumSalary;
+    }
+
+    public static void printFullNameEmployees(Employee[] employee) {
+        System.out.println("Список всех сотрудников Ф.И.Ф:");
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null) {
+                System.out.println(employee[i].getMiddleName() + " " + employee[i].getFirstName() + " " + employee[i].getLastName());
+            }
+        }
     }
 }
